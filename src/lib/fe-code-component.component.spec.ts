eea65aec4bb488309d3eaf4e00962d3da170689a
christian-theriot <christian@theriot.dev>
2022-07-11 19:21:20 -0400

    add test

--- src/lib/fe-code-component.component.spec.ts
+++ src/lib/fe-code-component.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { FeCodeComponentComponent } from './fe-code-component.component';
+import { FeCodeComponentComponent } from "./fe-code-component.component";
 
-describe('FeCodeComponentComponent', () => {
+describe("FeCodeComponentComponent", () => {
   let component: FeCodeComponentComponent;
   let fixture: ComponentFixture<FeCodeComponentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FeCodeComponentComponent ]
-    })
-    .compileComponents();
+      declarations: [FeCodeComponentComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FeCodeComponentComponent);
     component = fixture.componentInstance;
+    component.src = 'console.log("Hello World")';
+    component.async = true;
+
+    spyOn(console, "log").and.callFake(() => {});
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+    expect(console.log).toHaveBeenCalledWith("Hello World");
+  });
+
+  it("should be able to inject an arbitrary script", () => {
+    component.src = "let v = 10; console.log(v)";
+    component.ngAfterViewInit();
+
+    expect(console.log).toHaveBeenCalledWith(10);
   });
 });
